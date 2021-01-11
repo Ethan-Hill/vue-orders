d8a12deae20482079ce313605a595cc6d71c5457
Ethan-Hill <ethanhilljay@outlook.com>
2021-01-11 05:40:55 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
   <div id="app">
-  <Navbar />
-  <Orders v-if="this.$store.state.loggedIn"/>
-  <Welcome v-if="!this.$store.state.loggedIn"/>
+    <Navbar />
+    <Orders v-if="this.$store.state.loggedIn" />
+    <Welcome v-if="!this.$store.state.loggedIn" />
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar'
-import Orders from './components/Orders'
-import Welcome from './components/Welcome'
+import Navbar from "./components/Navbar";
+import Orders from "./components/Orders";
+import Welcome from "./components/Welcome";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Navbar,
     Orders,
-    Welcome
+    Welcome,
   },
   created() {
-    this.$store.state.loggedIn = false;
-  }
-}
+    this.$store.state.loggedIn = true;
+  },
+};
 </script>
 
 <style>
@@ -31,6 +31,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-
 }
 </style>
